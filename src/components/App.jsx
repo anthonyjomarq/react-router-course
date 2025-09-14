51ed2a996be24cc9025d4ab5070701b4eea6e6c1
Colon Dominguez, Anthony <anthonyjomar441@gmail.com>
2025-09-14 08:27:08 -0400

    URL Parameters

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,29 @@
-export default function App () {
+import * as React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useParams,
+} from "react-router-dom";
+
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Teams from "./Teams";
+import Players from "./Player";
+import TeamPage from "./TeamPage";
+
+export default function App() {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
-  )
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/players" element={<Players />} />
+          <Route path="/teams" element={<Teams />} />
+          <Route path="/:teamId" element={<TeamPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
